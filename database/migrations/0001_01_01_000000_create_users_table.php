5a3d091f7c00d845d370995fa467000d8d5b757c
conqueror <88179705+vu0cay@users.noreply.github.com>
2024-11-16 15:24:49 +0700

    update

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('name');
             $table->string('avatarUrl');
+            
 
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
